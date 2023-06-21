cf96bdbb11b6dc4b19bcecaa30133c75f81fdd36
ckotugeh <ckotugeh@gmail.com>
2023-06-22 00:36:52 +0300

    gui IntelGraphicsProfiles

---
+++ .vscode/GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root=Tk()
+    #create window dimensions
+
+    #create label
+
+myLabel= True.label(root, Text="going Global")
+#show on the screen
+myLabel.pack()
+#create a loop
+root.mainloop()
